6e71157ee28c8f99ceba9ce9e7f3f79cdc48751b
Parker Rebensdorf <parker.rebensdorf@gmail.com>
2018-07-23 13:29:29 -0700

    full prototype complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
+
+function luckyNumber() {
+    return Math.floor((Math.random()*1000) + 1);
+}
+
+const userObj = {
+    name: 'Paul',
+    number: luckyNumber()
+}
+
+function greeting(userObj) {
+    return (
+        <div className="container">
+            <h1>Welcome, {userObj.name}! </h1>
+            <h2>Your lucky number is: {userObj.number}</h2>
+        </div>
+    )
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    // <h1 className="container">React Prototypes!</h1>,
+    greeting(userObj),
     document.getElementById('root')
 );
