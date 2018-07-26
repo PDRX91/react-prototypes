7d1e522f66ac2e7007f53a515548bd4d171faf84
Parker Rebensdorf <parker.rebensdorf@gmail.com>
2018-07-26 15:06:36 -0700

    finished routing prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,19 @@
+import React from 'react'
+import Nav from './nav';
+import OurMacarons from './our_macarons';
+import Welcome from './welcome';
+import GiftsParties from './gift_parties';
+import Contact from './contact';
+import { Route } from 'react-router-dom';
 
-
-//test
+export default () => {
+    return(
+        <div className="container">
+            <Nav/>
+            <Route exact path="/" component={Welcome}/>
+            <Route path="/our-macarons" component={OurMacarons} />
+            <Route path="/gifts-parties" component={GiftsParties} />
+            <Route path="/contact" component={Contact} />
+        </div>
+    )
+}
---
+++ src/components/welcome.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1>This is my welcome page.</h1>
+        <p>Lorem Ipsum Dolor</p>
+    </div>
+)
